c8b0cde4e79c3945ed06f1e9e5bb2714a864dc6e
castrop@predictia.es <castrop@predictia.es>
2025-07-04 13:14:19 +0200

    fixing bridges system bug

--- src/main/java/eu/ai4eosc/provenance/api/tables/rmlbuilder/RMLBuilder.java
+++ src/main/java/eu/ai4eosc/provenance/api/tables/rmlbuilder/RMLBuilder.java
@@ -1,6 +1,5 @@
 package eu.ai4eosc.provenance.api.tables.rmlbuilder;
 
-import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.JsonNodeFactory;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -12,11 +11,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Scanner;
 
 /**
  * Builds an RML Fragments base on RML Generator FileSystem
@@ -46,21 +43,25 @@ public class RMLBuilder {
         String treePath = buildRMLTreePath(id);
         newOriginPair(origins, treePath + originFile);
 
+        log.info("reading core RML content with id {}  ... ", id);
         // Load CORE RML
         String coreRMLPath = treePath + contentFile;
         List<String> rmlLines = isReader.readResourceFileAsStringList(coreRMLPath);
 
+        log.info("coreRML reading complete");
+        log.info("Starting including extensions origins...");
         List<String> extensionsPath = extensions.stream()
                 .map(e -> buildExtensionPath(treePath, e)).toList();
-
         // Add origins
         for (String extP : extensionsPath) {
             newOriginPair(origins, extP + originFile); // add the extension origin to origins
         }
+        log.info("Extensions origins included");
 
         // Add BASE BRIDGEs TO CORE
         // Read all baseBridges
         // Then add them in descendant indexes order
+        log.info("adding base bridges to core...");
         List<String> baseBridgePaths = extensionsPath.stream().map(s -> s + bridgesSubDir + baseBridgeFile).toList();
         List<Bridge> descendantBridges = getAllBridgesDescendantOrder(baseBridgePaths);
 
@@ -70,7 +71,9 @@ public class RMLBuilder {
         }
 
         rmlLines.add("\n");
+        log.info("base bridges insertion to core completed");
         // Add conditional bridges for each extension
+        log.info("Loading bridges between extensions...");
         for (String extension : extensions) {
             List<String> externalExtensions = extensions.stream().filter(e -> !e.equals(extension)).toList();
             List<String> completeExtensionRML = buildExtensionRML(buildExtensionPath(treePath, extension), externalExtensions);
@@ -109,15 +112,17 @@ public class RMLBuilder {
     }
 
     List<Bridge> getAllBridgesDescendantOrder(List<String> bridgesPaths) throws IOException {
+        log.info("accesing bridges: {}", bridgesPaths);
         List<Bridge> allBridges = new ArrayList<Bridge>();
         for (String bridgePath : bridgesPaths) {
+            log.info("trying to access: {}", bridgePath);
             try (InputStream bridgeIS = getClass().getResourceAsStream(bridgePath)) {
                 if (bridgeIS != null) {
                     Bridges bridges = yamlMapper.readValue(bridgeIS, Bridges.class);
                     if (bridges.bridges() != null && !bridges.bridges().isEmpty())
                         allBridges.addAll(bridges.bridges());
                 } else {
-                    log.debug("bridgePath: {} does not exist", bridgePath);
+                    log.info("bridgePath: {} does not exist", bridgePath);
                 }
             }
         }
@@ -135,8 +140,9 @@ public class RMLBuilder {
      * @return
      */
     List<String> buildExtensionRML(String targetExtensionPath, List<String> potentialBridges) throws IOException {
+        log.info("Building extension {} for potential bridges: {}", targetExtensionPath, potentialBridges);
         // Get all the bridges for an extension (excluding base.yaml)
-        List<String> potentialBridgesPaths = potentialBridges.stream().map(pb -> targetExtensionPath + bridgesSubDir + pb).toList();
+        List<String> potentialBridgesPaths = potentialBridges.stream().map(pb -> targetExtensionPath + bridgesSubDir + "/" + pb + ".yaml").toList();
         List<Bridge> descendantBridges = getAllBridgesDescendantOrder(potentialBridgesPaths);
 
         // Load base extension and writes in it the extensions bridges ordered by descendant index order
